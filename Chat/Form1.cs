830bbdcb14b732cef7b4cba20a4c923bd6f80919
panting <fritzvillacorta@gmail.com>
2018-03-07 21:55:37 +0800

    Added encryption to messages

--- Chat/Form1.cs
+++ Chat/Form1.cs
@@ -15,6 +15,7 @@ namespace Chat
     {
         Socket suck;
         EndPoint eLocal, eRemote;
+        
 
         public Form1()
         {
@@ -24,8 +25,6 @@ namespace Chat
 
             txtOneIP.Text = GetLocalIP();
             txtTwoIP.Text = GetLocalIP();
-
-            //btnSend.Enabled = false;
         }
 
         private string GetLocalIP()
@@ -56,8 +55,11 @@ namespace Chat
                     receiveData = (byte[])result.AsyncState;
 
                     ASCIIEncoding e = new ASCIIEncoding();
+                    
                     string recievedMessage = e.GetString(receiveData);
-                    listConvo.Items.Add("Friend: " + recievedMessage);
+                    string get = decrypt(recievedMessage);
+                    listConvo.Items.Add("Friend (encrypted): " + recievedMessage);
+                    listConvo.Items.Add("Friend (decrypted): " + get);
                 }
 
                 byte[] buffer = new byte[1500];
@@ -65,7 +67,7 @@ namespace Chat
             }
             catch(Exception e)
             {
-                MessageBox.Show(e.ToString());
+                //MessageBox.Show(e.ToString());
             }
         }
 
@@ -81,16 +83,19 @@ namespace Chat
             {
                 System.Text.ASCIIEncoding enc = new System.Text.ASCIIEncoding();
                 byte[] msg = new byte[1500];
-                msg = enc.GetBytes(txtType.Text);
+
+                string pass = encrypt(txtType.Text);
+                msg = enc.GetBytes(pass);
 
                 suck.Send(msg);
-
-                listConvo.Items.Add("You: " + txtType.Text);
+                
+                listConvo.Items.Add("You (encrypted): " + pass);
+                listConvo.Items.Add("You (decrypted): " + txtType.Text);
                 txtType.Clear();
             }
             catch (Exception x)
             {
-                MessageBox.Show(x.ToString());
+                //MessageBox.Show(x.ToString());
             }
         }
 
@@ -122,8 +127,96 @@ namespace Chat
             }
             catch(Exception x)
             {
-                MessageBox.Show(x.ToString());
+                //MessageBox.Show(x.ToString());
             }
+        }
+
+        private string encrypt(string message)
+        {
+            string encrypted = "";
+            int length = message.Length;
+            char[] encrypted_message = new char[length];
+            char c;
+            bool flag = true;
+            int key = 2;
+
+            for (int i = 0; i < length; i++)
+            {
+                c = message[i];
+
+                if (c >= 'a' && c <= 'z')
+                {
+                    c = (char)(c + key);
+                    if (c > 'z')
+                    {
+                        c = (char)(c - 'z' + 'a' - 1);
+                    }
+
+                    encrypted_message[i] = c;
+                }
+                else if (c >= 'A' && c <= 'Z')
+                {
+                    c = (char)(c + key);
+                    if (c > 'Z')
+                    {
+                        c = (char)(c - 'Z' + 'A' - 1);
+                    }
+
+                    encrypted_message[i] = c;
+                }
+                else if (c == ' ')
+                {
+                    c = (char)(c + key);
+                    encrypted_message[i] = c;
+                }
+                else
+                {
+                    MessageBox.Show("No numbers or symbols allowed!", "Invalid input", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    flag = false;
+                    break;
+                }
+            }
+            if (flag == true)
+            {
+                encrypted = new String(encrypted_message);
+            }
+            else
+            {
+                encrypted = "";
+            }
+
+            return encrypted;
+        }
+
+        private string decrypt(string message)
+        {
+            string decrypted = "";
+            int length = message.Length;
+            char[] decrypted_message = new char[length];
+            char c;
+            int key = 2;
+
+            for (int i = 0; i < length; i++)
+            {
+                c = message[i];
+                c = (char)(c - key);
+
+                if (!(Char.IsLetter(c)) && !(c.Equals(' ')))
+                {
+                    c = (char)(c + 26);
+                }
+                decrypted_message[i] = c;
+            }
+
+            decrypted = new String(decrypted_message);
+
+            return decrypted;
+        }
+
+        private string hash(string s)
+        {
+            string hashed = "";
+            return hashed;
         }
     }
 }
